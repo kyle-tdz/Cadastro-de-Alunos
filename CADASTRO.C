ccefe5b3a25b73324035354a99e351e8ba318e76
Kaio Tadeu dos Santos Saraiva <kaio.tadeu.1800@gmail.com>
2025-12-02 15:16:49 +0000

    declarada algumas variáveis, darei continuidade depois

---
+++ CADASTRO.C
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(){
+    int idade; 
+    int quantidade = 1;
+    float altura = 1.75;
+    char letra = 'K';
+    char nome[4] = "kaio";
+
+    printf("Bem-vindo ao seu cadastro de Alunos!\n");
+    printf("--- Insira seus dados a seguit:. ---\n\n");
+
+   // --- LEITURA DA IDADE ---
+
+    printf("Digite a sua idade: ");
+    scanf("%d", &idade1);
+    printf("A idade é: %d\n", idade1);
+
+
+    // --- LEITURA DA ALTURA ---
+
+    printf("Digite a sua altura: ");
+    scanf ("%f", &altura1);
+    printf("A altura é: %.2f\n", altura1);
+
+
+}
